7fbec87c58fd4646db2eca69880949137afcf966
Bill Meyer <stinkjuice@gmail.com>
2018-06-20 12:03:11 -0500

    Converted to use page element bindings

--- src/test/java/io/billmeyer/saucelabs/parallel/CarLoanTest.java
+++ src/test/java/io/billmeyer/saucelabs/parallel/CarLoanTest.java
@@ -2,9 +2,11 @@ package io.billmeyer.saucelabs.parallel;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.By;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.InvalidElementStateException;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,9 +14,37 @@ import org.testng.annotations.Test;
 
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
 
 public class CarLoanTest extends TestBase
 {
+    @FindBy(id = "io.billmeyer.loancalc:id/etLoanAmount")
+    MobileElement etLoanAmount;
+
+    @FindBy(id = "io.billmeyer.loancalc:id/etEditInterest")
+    MobileElement etEditInterest;
+
+    @FindBy(id = "io.billmeyer.loancalc:id/etSalesTax")
+    MobileElement etSalesTax;
+
+    @FindBy(id = "io.billmeyer.loancalc:id/etTerm")
+    MobileElement etTerm;
+
+    @FindBy(id = "io.billmeyer.loancalc:id/etDownPayment")
+    MobileElement etDownPayment;
+
+    @FindBy(id = "io.billmeyer.loancalc:id/etTradeIn")
+    MobileElement etTradeIn;
+
+    @FindBy(id = "io.billmeyer.loancalc:id/etFees")
+    MobileElement etFees;
+
+    @FindBy(id = "io.billmeyer.loancalc:id/btnCalculate")
+    MobileElement btnCalculate;
+
+    @FindBy(id = "io.billmeyer.loancalc:id/tvLoanTotal")
+    MobileElement tvLoanTotal;
+
     /**
      * Runs a simple test verifying if the comment input is functional.
      *
@@ -27,20 +57,10 @@ public class CarLoanTest extends TestBase
 
         AndroidDriver driver = createDriver(platformName, platformVersion, deviceName, method.getName());
 
-        // Get the Input Elements...
-        MobileElement etLoanAmount = (MobileElement) driver.findElement(By.id("io.billmeyer.loancalc:id/etLoanAmount"));
-        MobileElement etEditInterest = (MobileElement) driver.findElement(By.id("io.billmeyer.loancalc:id/etEditInterest"));
-        MobileElement etSalesTax = (MobileElement) driver.findElement(By.id("io.billmeyer.loancalc:id/etSalesTax"));
-        MobileElement etTerm = (MobileElement) driver.findElement(By.id("io.billmeyer.loancalc:id/etTerm"));
-        MobileElement etDownPayment = (MobileElement) driver.findElement(By.id("io.billmeyer.loancalc:id/etDownPayment"));
-        MobileElement etTradeIn = (MobileElement) driver.findElement(By.id("io.billmeyer.loancalc:id/etTradeIn"));
-        MobileElement etFees = (MobileElement) driver.findElement(By.id("io.billmeyer.loancalc:id/etFees"));
-        MobileElement btnCalculate = (MobileElement) driver.findElement(By.id("io.billmeyer.loancalc:id/btnCalculate"));
+        // Initialize our MobileElement variable bindings...
+        PageFactory.initElements(new AppiumFieldDecorator(driver, 5, TimeUnit.SECONDS), this);
 
-        // Get the Output Elements...
-        MobileElement tvLoanTotal = (MobileElement) driver.findElement(By.id("io.billmeyer.loancalc:id/tvLoanTotal"));
-
-        /* Add two and two. */
+        // Set the input values for our loan calculation...
         etLoanAmount.sendKeys("25000");
         etEditInterest.sendKeys("3.42");
         etSalesTax.sendKeys("8");
@@ -53,10 +73,10 @@ public class CarLoanTest extends TestBase
         btnCalculate.click();
         driver.getScreenshotAs(OutputType.FILE);
 
-        /* Check if within given time the correct result appears in the designated field. */
+        // Check if within given time the correct result appears in the designated field.
         ExpectedCondition<Boolean> expected = ExpectedConditions.textToBePresentInElement(tvLoanTotal, "20,370.97");
 
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebDriverWait wait = new WebDriverWait(driver, 30);
         try
         {
             wait.until(expected);
